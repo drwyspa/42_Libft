5364652d28955acb83738fe87d6b79688adbe4d5
Przemysław Jędryczkowski <86292163+drwyspa@users.noreply.github.com>
2024-03-25 22:20:30 +0100

    Add files via upload

--- ft_memset.c
+++ ft_memset.c
@@ -6,21 +6,34 @@
 /*   By: pjedrycz <p.jedryczkowski@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/16 12:47:42 by pjedrycz          #+#    #+#             */
-/*   Updated: 2024/03/22 19:49:23 by pjedrycz         ###   ########.fr       */
+/*   Updated: 2024/03/25 22:12:00 by pjedrycz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// #include <stdio.h>
+// #include <string.h>
 #include "libft.h"
 
 void	*ft_memset(void *s, int c, size_t n)
 {
-	unsigned char	*i;
+	unsigned char	*temp;
 
-	i = (unsigned char *) s;
+	temp = (unsigned char *) s;
 	while (n > 0)
 	{
-		*(i) = (unsigned char) c;
+		*(temp++) = (unsigned char) c;
 		n--;
 	}
 	return (s);
 }
+
+// int	main()
+// {
+// 	char s[100] = "aaaaa";
+// 	char sbis[100] = "aaaaa";
+// 	if (!strcmp(memset(s, 'z', 3), ft_memset(sbis, 'z', 3)))
+// 		printf("OK\n");
+// 	else
+// 		printf("Failed : expected '%s', got '%s'.\n", s, sbis);
+// 	return (0);
+// }
